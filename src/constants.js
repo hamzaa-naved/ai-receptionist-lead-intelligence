5fc627985dddb9304d3295354c5e61194b345374
Hamza Naveed <hamzaa.naveed@outlook.com>
2026-08-18 07:24:20 -0700

    Add src/constants.js

---
+++ src/constants.js
@@ -0,0 +1,34 @@
+export const ALL_US_STATES = [
+  'Alabama','Alaska','Arizona','Arkansas','California','Colorado','Connecticut','Delaware','Florida','Georgia',
+  'Hawaii','Idaho','Illinois','Indiana','Iowa','Kansas','Kentucky','Louisiana','Maine','Maryland','Massachusetts',
+  'Michigan','Minnesota','Mississippi','Missouri','Montana','Nebraska','Nevada','New Hampshire','New Jersey',
+  'New Mexico','New York','North Carolina','North Dakota','Ohio','Oklahoma','Oregon','Pennsylvania','Rhode Island',
+  'South Carolina','South Dakota','Tennessee','Texas','Utah','Vermont','Virginia','Washington','West Virginia',
+  'Wisconsin','Wyoming','District of Columbia'
+];
+
+export const JOB_DOMAINS = [
+  'indeed.com','linkedin.com','ziprecruiter.com','glassdoor.com','careerbuilder.com','monster.com',
+  'workstream.us','careerplug.com','applicantpro.com','bamboohr.com','paylocity.com','paycomonline.net',
+  'greenhouse.io','lever.co','workable.com','smartrecruiters.com','jazzhr.com','applytojob.com',
+  'jobvite.com','icims.com','recruitee.com','breezy.hr','ashbyhq.com','jobright.ai','getonbrd.com',
+  'simplyhired.com','talent.com','jooble.org','wayup.com','qureos.com','onlinejobs.ph'
+];
+
+export const SOCIAL_DOMAINS = [
+  'facebook.com','instagram.com','linkedin.com','youtube.com','x.com','twitter.com','tiktok.com'
+];
+
+export const QUERY_FAMILIES = [
+  '("customer service representative" OR CSR OR receptionist OR "customer care" OR "call taker")',
+  '("service dispatcher" OR dispatcher OR "scheduling coordinator" OR scheduler OR "office assistant" OR "service coordinator")'
+];
+
+export const NICHE_QUERY = {
+  electrical: '(electrician OR "electrical contractor" OR "electrical service" OR "electric company")',
+  hvac: '(HVAC OR "heating and cooling" OR "air conditioning")',
+  plumbing: '(plumber OR plumbing)',
+  roofing: '(roofer OR roofing)',
+  restoration: '(restoration OR "water damage" OR "fire damage")',
+  garage_door: '("garage door")'
+};
